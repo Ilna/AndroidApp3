d675351832634c6916b350764e493854174be1af
Ilia Malfa <[Dit214166@hua.gr>
2019-02-15 10:32:38 +0200

    Select Location by Clicking and Insert it to the DB,exam query2

--- app/src/main/java/com/example/android/secondassigmentapp3/MapsActivity.java
+++ app/src/main/java/com/example/android/secondassigmentapp3/MapsActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.secondassigmentapp3;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -12,7 +15,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -35,7 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         List<Marker> List = new ArrayList<>();
         //get the userid
         Intent intent = getIntent();
-        String userid = intent.getStringExtra("userid");
+        final String userid = intent.getStringExtra("userid");
         String selection = "_USERID=?";
         String [] selectionArgs = new String[]{userid};
 
@@ -52,6 +57,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             List.add(pin);
 
             }
-        }
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                System.out.println("CLICKED");
+                Marker pin = mMap.addMarker(new MarkerOptions().position(latLng));
+                double latitude = latLng.latitude;
+                double longitude = latLng.longitude;
+                System.out.println(latitude+"    "+longitude);
+
+                SimpleDateFormat s = new SimpleDateFormat("dd/MM/yyyy,hh:mm:ss");
+                String currentTimestamp = s.format(new Date());
+
+
+                //insert the values to the values object
+                ContentValues values = new ContentValues();
+                values.put("_USERID",userid);
+                values.put("_LONGTITUDE",longitude);
+                values.put("_LATITUDE",latitude);
+                values.put("timeStamp",currentTimestamp);
+                System.out.println(values);
+                //send the values to the first assigment content provider
+                getContentResolver().insert(Uri.parse("content://com.example.android.firstassignment/datatable"),values);
+            }
+
+
+        });
+
+
+
     }
+
+}
+
+
 
